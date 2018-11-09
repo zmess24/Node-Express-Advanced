19b22ffcfdd17048d1cca806e8be9d37153595c7
Zac Messinger <zac.messinger@gmail.com>
2018-11-08 22:07:34 -0800

    Updated Key

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: process.env.MONGODB_URI,
+  mongoURI: 'mongodb://zdm_admin:wanted09@ds157383.mlab.com:57383/blog_dev',
   cookieKey: '123123123'
 };
