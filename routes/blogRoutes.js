2a75a4f1634c27171fcb1cef610b3dfd9bfc1f7c
Zac Messinger <zac.messinger@gmail.com>
2018-11-12 19:18:21 -0800

    Added caching via for '/api/blogs route'

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,27 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis  = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    // Standard library included in Node runtime that has utility functions we can use.
+    const util = require('util'); 
+    client.get = util.promisify(client.get);
+    // Do we have any cached data in redis related to this query?
+    // REMEMBER: Cant use the async/await syntax here with promisifying the function.
+    const cachedBlogs = await client.get(req.user.id); 
+    // if yes, then respond to the request right away
+    // and return
+    if (cachedBlogs) { 
+      console.log("Serving from cache");
+      return res.send(cachedBlogs);
+    }
+    // if no, we need to respont to request
+    // and update our cache to store the data
     const blogs = await Blog.find({ _user: req.user.id });
-
     res.send(blogs);
+    // REMEMBER: Redis can't store objects, hence `JSON.stringify`.
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
