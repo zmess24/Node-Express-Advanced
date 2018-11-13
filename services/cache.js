23383c1c30d072bf17b0195e8403e1da64ca1249
Zac Messinger <zac.messinger@gmail.com>
2018-11-12 21:10:33 -0800

    Cleaned up contoller action

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,6 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
     console.log("IM ABOUT TO RUN A QUERY");
+    console.log(this.getQuery);
     return exec.apply(this, arguments);
-}
+};
