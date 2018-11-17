564af4a8fc59d8792261aa9f150160c5b1cea0f9
Zac Messinger <zac.messinger@gmail.com>
2018-11-16 19:39:11 -0800

    Added lastest change

--- tests/header.test.js
+++ tests/header.test.js
@@ -5,19 +5,19 @@ const puppeteer = require("puppeteer");
 let browser, page;
 
 // beforeEach gest invoked before every test.
-beforeEach(() => { 
+beforeEach(async () => { 
     // Opens up chromium browser
-    browser = await puppeteer.launch({
-      headless: false
-    });
-
+    browser = await puppeteer.launch({ headless: false });
     // Open new tab and navigate to localhost:3000
     page = await browser.newPage();
     await page.goto('localhost:3000');
-})
+});
+
+afterEach(async() => {
+    await browser.close();
+});
 
 test('We can launch a browser', async () => {
-    const text = page.$eval('a.brand-logo', el => el.innerHTML);
-
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 });
