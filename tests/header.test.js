b85415c1eec3ced80af0af4f6ecd760b8febaaf1
Zac Messinger <zac.messinger@gmail.com>
2018-11-16 16:31:17 -0800

    Added Puppeteer 'into header.test.js' file

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,15 @@
+// Puppateer starts up Chromium
+const puppeteer = require("puppeteer");
+
 test('Adds two numbers', () => { // Description of test
     const sum = 1 + 2;
     expect(sum).toEqual(3); // Equivilent of assert
+});
+
+test('We can launch a browser', async () => {
+    // Opens up chromium browser
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
 });
