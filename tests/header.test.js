11e1bdf22ce797cc1020473a234fc35991abfddc
Zac Messinger <zac.messinger@gmail.com>
2018-11-16 16:38:20 -0800

    Added chromium navigation to localhost:3000

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,5 +11,7 @@ test('We can launch a browser', async () => {
     const browser = await puppeteer.launch({
         headless: false
     });
+
     const page = await browser.newPage();
+    await page.goto('localhost:3000');
 });
