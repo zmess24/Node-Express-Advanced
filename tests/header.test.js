3054f64d93b8f3f9d2dbf9c04f4fa558f9b2fba0
Zac Messinger <zac.messinger@gmail.com>
2018-11-16 20:58:52 -0800

    Added oauth test

--- tests/header.test.js
+++ tests/header.test.js
@@ -17,7 +17,13 @@ afterEach(async() => {
     await browser.close();
 });
 
-test('We can launch a browser', async () => {
+test('the header has the correct test', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts the oauth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/)
 });
