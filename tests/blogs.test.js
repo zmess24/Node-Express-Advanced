d8e835bb1f6374eae595fa966ad83aaacbfd4e7f
Zac Messinger <zac.messinger@gmail.com>
2018-11-23 19:30:29 -0800

    Successfully finished tests for using valid inputs

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -22,6 +22,33 @@ describe('When logged in', async () => {
         expect(label).toEqual('Blog Title');
     });
 
+    describe('And using valid inputs', async () => {
+        beforeEach(async () => {
+            // Attempt to enter text into inputs and submit.
+            await page.type('.title input', 'My Title');
+            await page.type('.content input', 'My Content');
+            await page.click('form button');
+        });
+
+        test('Submitting takes user to review screen', async () => {
+            // Checking page is correct by checking h5 text.
+            const text = await page.getContentsOf('h5');
+            expect(text).toEqual('Please confirm your entries');
+        });
+
+        test('Submitting then saving adds blog to index page', async () => {
+            await page.click('button.green');
+            // Wait for DOM contents to finish loading.
+            await page.waitFor('.card');
+
+            const title = await page.getContentsOf('.card-title');
+            const content = await page.getContentsOf('p');
+
+            expect(title).toEqual('My Title');
+            expect(content).toEqual('My Content');
+        });
+    });
+
     describe('And using invalid inputs', async () => {
         beforeEach(async () => {
             await page.click('form button');
