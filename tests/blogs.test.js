a11b7d5a27cc472fe810d71df9d2577693a9b29d
Zac Messinger <zac.messinger@gmail.com>
2018-11-23 19:16:21 -0800

    Added 'And using invalid inputs' describe block with test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -17,8 +17,22 @@ describe('When logged in', async () => {
         await page.click('a.btn-floating');
     });
 
-    test('can see blog craete form', async () => {
+    test('can see blog create form', async () => {
         const label = await page.getContentsOf('form label');
         expect(label).toEqual('Blog Title');
     });
-})
+
+    describe('And using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+    });
+});
