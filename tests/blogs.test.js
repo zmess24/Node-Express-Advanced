65069ef67bbc39bc28e3a3dab491cf3a916a151a
Zac Messinger <zac.messinger@gmail.com>
2018-11-26 21:50:58 -0800

    Added 'User cannot get a list of posts' test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -76,5 +76,17 @@ describe('User is not logged in', async () => {
             }).then(res => res.json());
         })
         expect(result).toEqual({ error: "You must log in!" });
-    })
+    });
+
+    test('User cannot get a list of posts', async () => {
+        const result = await page.evaluate(() => {
+            // 'fetch' api returns raw data, need to conver to json
+            return fetch('/api/blogs', {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: { 'Content-Type': 'application/json' }
+            }).then(res => res.json());
+        });
+        expect(result).toEqual({ error: "You must log in!" });
+    });
 });
