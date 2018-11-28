ccc51a48ee1985088c77a78c4387e9d9d9139522
Zac Messinger <zac.messinger@gmail.com>
2018-11-28 15:31:29 -0800

    Added ci.js key env variables

---
+++ config/ci.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://zdm_admin:wanted09@ds157383.mlab.com:57383/blog_dev',
+  cookieKey: '123123123' // Secret key for signing the base64 session encoding.
+};
