a851599444478487497b30664ded6d30a3ebd358
Zac Messinger <zac.messinger@gmail.com>
2018-11-27 21:30:36 -0800

    Added dynamic api testing method 'execRequests' to CustomPage class

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -65,13 +65,35 @@ describe('When logged in', async () => {
 });
 
 describe('User is not logged in', async () => {
-    test('User cannot create blog posts', async () => {
-        const result = await page.post('/api/blogs', { title: 'My Title', content: 'My Content' })
-        expect(result).toEqual({ error: "You must log in!" });
-    });
+    const actions = [
+        {
+            method: 'get',
+            path: '/api/blogs',
+        },
+        {
+            method: 'post',
+            path: '/api/blogs',
+            data: {
+                title: 'Title',
+                content: 'Content'
+            }
+        }
+    ];
 
-    test('User cannot get a list of posts', async () => {
-        const result = await page.get('/api/blogs');
-        expect(result).toEqual({ error: "You must log in!" });
+    test('Blog related actions are prohibited', async () => {
+        const results = await page.execRequests(actions);
+
+        for (let result of results) {
+            expect(result).toEqual({ error: 'You must log in!'})
+        }
     });
+    // test('User cannot create blog posts', async () => {
+    //     const result = await page.post('/api/blogs', { title: 'My Title', content: 'My Content' })
+    //     expect(result).toEqual({ error: "You must log in!" });
+    // });
+
+    // test('User cannot get a list of posts', async () => {
+    //     const result = await page.get('/api/blogs');
+    //     expect(result).toEqual({ error: "You must log in!" });
+    // });
 });
