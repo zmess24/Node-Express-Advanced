9bfc7182d439e772d470df6dca40171d2b900da2
Zac Messinger <zac.messinger@gmail.com>
2018-11-29 21:08:19 -0800

    Added image upload button

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,9 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+
+  state = { file: null };
+  
   renderFields() {
     const { formValues } = this.props;
 
@@ -41,10 +44,12 @@ class BlogFormReview extends Component {
 
   onSubmit(event) {
     event.preventDefault();
-
     const { submitBlog, history, formValues } = this.props;
+    submitBlog(formValues, this.state.file, history);
+  }
 
-    submitBlog(formValues, history);
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
   }
 
   render() {
@@ -52,7 +57,11 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
-
+        <h5>Add an Image</h5>
+        <input 
+          onChange={this.onFileChange.bind(this)}
+          type="file" 
+          accept="image/*"/>
         {this.renderButtons()}
       </form>
     );
