75d2b19e078aa7ba4bf27968836ee8f9a5648fe5
Zac Messinger <zac.messinger@gmail.com>
2018-11-30 14:12:49 -0800

    hid AWS keys

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://zdm_admin:wanted09@ds157383.mlab.com:57383/blog_dev',
   cookieKey: '123123123', // Secret key for signing the base64 session encoding.
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: process.env.AWS_ACCCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
